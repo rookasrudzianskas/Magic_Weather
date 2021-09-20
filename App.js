bc74b4c13ddf587f7efbb7d65d2f1027dd79f779
Rokas <rookas.rudzenskas@gmail.com>
2021-09-20 10:12:43 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,11 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- * @author Vadim Savin
- */
+
 
 import React from 'react';
 import { StatusBar } from 'react-native';
