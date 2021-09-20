412c4bd7e6ef78319389a7d3f6f49afd492387ba
Rokas <rookas.rudzenskas@gmail.com>
2021-09-20 08:32:15 +0300

    Rokas

--- src/screens/WeatherScreen/index.js
+++ src/screens/WeatherScreen/index.js
@@ -1,9 +1,7 @@
-
-
 import React, { useState } from 'react';
 import { View, Text, Pressable, Alert } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
 import { Credits } from '../../components';
 import { testCold, generateSampleData, Environment } from '../../Helpers/SampleData';
 import styles from './styles.js';
@@ -14,31 +12,31 @@ import { ENTITLEMENT_ID } from '../../constants';
  */
 
 const WeatherScreen = () => {
-  const [weatherData, setWeatherData] = useState(testCold);
+    const [weatherData, setWeatherData] = useState(testCold);
 
-  const navigation = useNavigation();
+    const navigation = useNavigation();
 
-  const performMagic = async () => {
-    // TODO  We should check if we can magically change the weather (subscription active) and if not, display the paywall.
-    setWeatherData(generateSampleData(Environment.EARTH));
-  };
+    const performMagic = async () => {
+        // TODO  We should check if we can magically change the weather (subscription active) and if not, display the paywall.
+        setWeatherData(generateSampleData(Environment.EARTH));
+    };
 
-  return (
-    <View style={[styles.page, { backgroundColor: weatherData.weatherColor }]}>
-      {/* Sample weather details */}
-      <Text style={styles.emoji}>{weatherData.emoji}</Text>
-      <Text style={styles.temperature}>
-        {weatherData.temperature}°{weatherData.unit.toUpperCase()}️
-      </Text>
+    return (
+        <View style={[styles.page, { backgroundColor: weatherData.weatherColor }]}>
+            {/* Sample weather details */}
+            <Text style={styles.emoji}>{weatherData.emoji}</Text>
+            <Text style={styles.temperature}>
+                {weatherData.temperature}°{weatherData.unit.toUpperCase()}️
+            </Text>
 
-      {/* The magic button that is disabled behind our paywall */}
-      <Pressable onPress={performMagic} style={styles.changeWeatherButton}>
-        <Text style={styles.changeWeatherTitle}>✨ Change the Weather</Text>
-      </Pressable>
+            {/* The magic button that is disabled behind our paywall */}
+            <Pressable onPress={performMagic} style={styles.changeWeatherButton}>
+                <Text style={styles.changeWeatherTitle}>✨ Change the Weather</Text>
+            </Pressable>
 
-      <Credits />
-    </View>
-  );
+            <Credits />
+        </View>
+    );
 };
 
 export default WeatherScreen;
