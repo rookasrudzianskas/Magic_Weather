a7f2e365c1dfb8d5eba4ca740e7b04ef59fd794a
Rokas <rookas.rudzenskas@gmail.com>
2021-09-20 10:00:38 +0300

    Rokas

--- android/app/src/main/java/com/rokasweatherapp/MainActivity.java
+++ android/app/src/main/java/com/rokasweatherapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.magicweatherreactnative;
+package com.rokasweatherapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "MagicWeatherRN";
+    return "rokasweatherapp";
   }
 }
--- ios/rokasweatherappTests/rokasweatherappTests.m
+++ ios/rokasweatherappTests/rokasweatherappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MagicWeatherRNTests : XCTestCase
+@interface rokasweatherappTests : XCTestCase
 
 @end
 
-@implementation MagicWeatherRNTests
+@implementation rokasweatherappTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
